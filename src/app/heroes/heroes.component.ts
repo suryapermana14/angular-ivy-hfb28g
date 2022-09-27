625ca320bf4d54b2c619f9df7da9903fd6792d7f
suryapermana14 <109714516+suryapermana14@users.noreply.github.com>
2022-09-27 15:21:53 +0800

    2 files modified

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,6 +7,10 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   heroes = HEROES;
   hero: Hero = {
     id: 1,
