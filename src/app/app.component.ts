b00e4e29250a42e9a37b2b10e72140b063ed8a46
suryapermana14 <109714516+suryapermana14@users.noreply.github.com>
2022-09-27 14:48:08 +0800

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Babi';
+export class AppComponent {
+  title = 'Tour Of Heroes';
 }
